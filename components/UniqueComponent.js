9405b059078a60b1693c11ccac346045bc514a91
Ula Chin <ula.chin092@gmail.com>
2021-11-10 04:32:56 -0500

    testing

---
+++ components/UniqueComponent.js
@@ -0,0 +1,30 @@
+
+import { render } from 'react-dom';
+import { UNIQUEITEMS } from '../shared/uniqueItems';
+
+
+class Unique extends Components {
+    constructor(props){
+        super(props);
+        this.state =(
+            UNIQUEITEMS
+        );
+
+    render() {
+ 
+    }    
+    return(
+        <View>
+            <Card>
+                <Text>
+
+                </Text>
+            </Card>
+        </View>
+    )
+    }
+}
+
+
+
+export default Unique; 
---
+++ shared/uniqueItems.js
@@ -0,0 +1,23 @@
+export const uniqueItems = [
+    {
+        id: 0,
+        name: "ThinSlim foods Keto Bagels",
+        image: "components/images/KetoBagel.png",      
+    },
+    {
+        id: 1,
+        name: "Keto Hot Dog Buns",
+        image: "components/images/KetoBuns.jpg",      
+    },
+    {
+        id: 2,
+        name: "",
+        image: "",      
+    },
+    {
+        id:3,
+        name: "La Banderita Carb Counter Street Taco",
+        image: "components/images/Ketotortilla.jpg",
+    },
+
+] 
